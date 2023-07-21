a7a8e19054234dc8fd96020196ff8329558c3dc2
EwelinaManiecka <ewelina.maniecka@op.pl>
2023-07-21 21:53:53 +0200

    add section Hero with computer rotation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { ComputersCanvas } from "./canvas";
 import { styles } from "../styles";
 
