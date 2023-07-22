92422fa6243519f2a42548523237962d7c8bdf4d
EwelinaManiecka <ewelina.maniecka@op.pl>
2023-07-22 16:30:48 +0200

    add my skills, my experience and my repos

--- src/constants/index.js
+++ src/constants/index.js
@@ -19,6 +19,7 @@ import {
   starbucks,
   tesla,
   shopify,
+  stanislaw,
   carrent,
   jobit,
   tripguide,
@@ -108,65 +109,67 @@ const technologies = [
     name: "figma",
     icon: figma,
   },
-  {
-    name: "docker",
-    icon: docker,
-  },
+  // {
+  //   name: "docker",
+  //   icon: docker,
+  // },
 ];
 
 const experiences = [
   {
-    title: "React.js Developer",
-    company_name: "Starbucks",
+    title: "Teacher",
+    company_name: "Primary school and secondary school",
     icon: starbucks,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "February 2010 - August 2019",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Teaching children and young people.",
+      "Collaborating with parents of children and young people.",
+      "Colaborating with teachers and the local community.",
+      "Organizing leisure time for children and teenagers.",
     ],
   },
   {
-    title: "React Native Developer",
-    company_name: "Tesla",
+    title: "Personal data administrator",
+    company_name: "Klinika małżeńska",
     icon: tesla,
     iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
+    date: "December 2019 - February 2020",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Patient data managment.",
+      "Collaborating with psychologists and psychotherapists.",
+      "Managing the dates of therapy meetings.",
+      "Summary of daily income and expenses of company.",
+      "Training of new employees.",
     ],
   },
   {
-    title: "Web Developer",
-    company_name: "Shopify",
-    icon: shopify,
+    title: "Editor",
+    company_name: "Wydawnictwo św. Stanisława BM",
+    icon: stanislaw,
     iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
+    date: "February 2020 - present",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Editing texts for textbooks.",
+      "Collaborating with editors, text authors, cartoonists, ilustrators, proofreaders and typesetters.",
+      "Taking care of the high quality of texts for textbooks.",
+      "Writing texts, correcting them.",
+      "Graphic design preparation.",
     ],
   },
-  {
-    title: "Full stack Developer",
-    company_name: "Meta",
-    icon: meta,
-    iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
+  // {
+  //   title: "Full stack Developer",
+  //   company_name: "Meta",
+  //   icon: meta,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2023 - Present",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
 ];
 
 const testimonials = [
@@ -198,12 +201,33 @@ const testimonials = [
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "Country search",
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      "Web-based platform that allows users to search country and basic information about this.",
     tags: [
       {
         name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "react",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://ewelinamaniecka.github.io/country-search-new/",
+  },
+  {
+    name: "Web tropical paradise",
+    description:
+      "Webpage with information, contact and advertisment some company.",
+    tags: [
+      {
+        name: "html, css",
         color: "blue-text-gradient",
       },
       {
@@ -216,12 +240,31 @@ const projects = [
       },
     ],
     image: carrent,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://ewelinamaniecka.github.io/tropical-paradise/",
   },
   {
-    name: "Job IT",
-    description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    name: "Car Rent",
+    description: "Webpage built in bootstrap 5",
+    tags: [
+      {
+        name: "html, css, bootstrap 5",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://ewelinamaniecka.github.io/bootstrap-page/",
+  },
+  {
+    name: "Space X",
+    description: "Web application with information about space travel.",
     tags: [
       {
         name: "react",
@@ -237,15 +280,15 @@ const projects = [
       },
     ],
     image: jobit,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://ewelinamaniecka.github.io/space-x/",
   },
   {
     name: "Trip Guide",
     description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      "Basic aplication when we can add (delete and change) something what we should do. .",
     tags: [
       {
-        name: "nextjs",
+        name: "react",
         color: "blue-text-gradient",
       },
       {
@@ -258,7 +301,68 @@ const projects = [
       },
     ],
     image: tripguide,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://ewelinamaniecka.github.io/todo-app-tasks/",
+  },
+  {
+    name: "Digital clock",
+    description: "Webpage with digital clock with many colours.",
+    tags: [
+      {
+        name: "html",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "css",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://ewelinamaniecka.github.io/digital-clock/",
+  },
+  {
+    name: "Trip Guide",
+    description: "Memory game build in react.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "css",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://ewelinamaniecka.github.io/memory-game-react/",
+  },
+  {
+    name: "Trip Guide",
+    description: "Basic weather application.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link:
+      "https://ewelinamaniecka.github.io/weather-app-basic-version/",
   },
 ];
 
