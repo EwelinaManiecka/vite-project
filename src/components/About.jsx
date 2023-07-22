4aa97793a7d7956e1f685f3029910b9e073a6d18
EwelinaManiecka <ewelina.maniecka@op.pl>
2023-07-22 12:59:42 +0200

    add card with my skills

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,58 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion.js";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px]"
+        ></div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 export const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm a junior front-end developer. I like React, Node.js and I learn
+        Three.js. Soon I want to lern TypeScript as well. I talk Spanish
+        fluently and English good. I'm from Poland and in this case, I talk
+        fluently Polish. I was a teacher, I work very well with people. I learn
+        new skills quickly. I was also an editor of books, the basics of UX/UI
+        are no strangers to me. I olso like to draw. I konw the basics of vector
+        graphics and photo processing. I would like to acquire knowledge and
+        experience working alongside the best programmers.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
 };
