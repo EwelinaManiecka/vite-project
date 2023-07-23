d8b77a00fb6414c175d97ff6f8879ee978deddd4
EwelinaManiecka <ewelina.maniecka@op.pl>
2023-07-23 12:54:55 +0200

    add EarthCanvas in the section contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -77,9 +77,15 @@ const Contact = () => {
             type="submit"
             className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
           >
-            Send
+            {loading ? "Sending..." : "Send"}
           </button>
         </form>
+      </motion.div>
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
       </motion.div>
     </div>
   );
