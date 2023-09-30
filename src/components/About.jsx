16fab2bee68f6123b45427c3abb70a24264121f9
EwelinaManiecka <105506837+EwelinaManiecka@users.noreply.github.com>
2023-09-30 20:55:19 +0200

    Update About.jsx
    Change front-end to fullstack

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a junior front-end developer. I like React, Node.js and I learn
+        I'm a junior fullstack developer. I like React, Node.js, SQL and I learn
         Three.js. Soon I want lern TypeScript as well. I speak Spanish fluently
         and English well. I come from Poland and therefore I speak Polish
         fluently. I was a teacher and I work very well with people. I learn new
