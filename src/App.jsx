04192417d20efad2a5de475c1c5bf4776972c623
EwelinaManiecka <105506837+EwelinaManiecka@users.noreply.github.com>
2023-09-30 20:59:05 +0200

    Update App.jsx
    Comment feedbacks

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import { BrowserRouter } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
-import Feedbacks from "./components/Feedbacks";
+// import Feedbacks from "./components/Feedbacks";
 import { Hero } from "./components/Hero";
 import { Navbar } from "./components/Navbar";
 import Tech from "./components/Tech";
@@ -36,7 +36,6 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
