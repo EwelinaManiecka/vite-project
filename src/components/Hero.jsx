35aa3c180f9c5d1b9cc0f3bfd49e4fca5e0d9df7
EwelinaManiecka <105506837+EwelinaManiecka@users.noreply.github.com>
2025-07-13 08:23:37 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ export const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915eff]">Ewelina</span>
+            Cześć, jestem <span className="text-[#915eff]">Ewelina</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Jestem pedagogiem, logoterapeutą, <br className="sm:block hidden" />{" "}
